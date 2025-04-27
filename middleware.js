ec019d25b51785db703c6c42e5e26cbee8c98803
dayatalamsyah <dayatalamsyah@gmail.com>
2025-04-27 07:25:50 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,7 +3,10 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
   const isAdmin = request.cookies.get('isAdmin')?.value || '';
 
-  if (request.nextUrl.pathname.startsWith('/admin') && !isAdmin) {
+  const { pathname } = request.nextUrl;
+
+  // Biar /admin/login boleh diakses tanpa harus login
+  if (pathname.startsWith('/admin') && pathname !== '/admin/login' && !isAdmin) {
     return NextResponse.redirect(new URL('/admin/login', request.url));
   }
 
