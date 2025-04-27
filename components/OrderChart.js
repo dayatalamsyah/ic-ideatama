da32c5645a4a1c6bdcdb3555b81a897ebecf8b7c
dayatalamsyah <dayatalamsyah@gmail.com>
2025-04-28 02:48:33 +0700

    Update OrderChart.js

--- components/OrderChart.js
+++ components/OrderChart.js
@@ -1,18 +1,19 @@
 'use client';
 
-import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 export default function OrderChart({ data }) {
   return (
     <div className="bg-white shadow rounded-lg p-6 mb-12">
-      <h2 className="text-xl font-bold mb-4 text-center">Grafik Order Masuk per Bulan</h2>
+      <h2 className="text-2xl font-bold mb-6 text-center">Statistik Order Masuk</h2>
       <ResponsiveContainer width="100%" height={300}>
-        <LineChart data={data}>
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="bulan" />
-          <YAxis allowDecimals={false} />
+          <YAxis />
           <Tooltip />
-          <Line type="monotone" dataKey="jumlah" stroke="#f97316" strokeWidth={3} />
-        </LineChart>
+          <Bar dataKey="jumlah" fill="#f97316" radius={[10, 10, 0, 0]} />
+        </BarChart>
       </ResponsiveContainer>
     </div>
   );
